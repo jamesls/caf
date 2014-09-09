13e0eb5f9e85857851edf12e19efecbae6054864
James Saryerwinnie <js@jamesls.com>
2014-09-08 22:20:28 -0700

    Refactor step definition to remove duplicate code

--- features/steps/base.py
+++ features/steps/base.py
@@ -6,6 +6,17 @@ from hamcrest import assert_that, equal_to
 import tempfile
 
 
+def get_all_generated_files(rootdir):
+    for root, _, filenames in os.walk(rootdir):
+        if '.metadata' in root:
+            # Don't iterate over the files in .metadata,
+            # these aren't the randomly generates files.
+            continue
+        for filename in filenames:
+            full_path = os.path.join(root, filename)
+            yield full_path
+
+
 class CommandResult(object):
     def __init__(self, stdout, stderr, rc):
         self.stdout = stdout
@@ -36,15 +47,9 @@ def step_impl(context, command):
 @then(u'the total disk usage of files generated should be {total_size}')
 def step_impl(context, total_size):
     disk_usage = 0
-    for root, _, filenames in os.walk(context.working_dir):
-        if '.metadata' in root:
-            # Don't count the metadata dir against the total
-            # disk usage.
-            continue
-        for filename in filenames:
-            full_path = os.path.join(root, filename)
-            file_size = os.stat(full_path).st_size
-            disk_usage += file_size
+    for full_path in get_all_generated_files(context.working_dir):
+        file_size = os.stat(full_path).st_size
+        disk_usage += file_size
 
     assert_that(int(total_size), equal_to(disk_usage))
     assert_that(disk_usage, equal_to(int(total_size)))
@@ -53,37 +58,21 @@ def step_impl(context, total_size):
 @then(u'the total number of files created should be {num_files}')
 def step_impl(context, num_files):
     actual_count = 0
-    for root, _, filenames in os.walk(context.working_dir):
-        if '.metadata' in root:
-            # Don't count the metadata dir against the total
-            # number of files.
-            continue
-        actual_count += len(filenames)
+    for full_path in get_all_generated_files(context.working_dir):
+        actual_count += 1
     assert_that(actual_count, equal_to(int(num_files)))
 
 
 @then(u'the size of each generated file should be between {min_file_size} and {max_file_size}')
 def step_impl(context, min_file_size, max_file_size):
-    for root, _, filenames in os.walk(context.working_dir):
-        if '.metadata' in root:
-            # Don't count the metadata dir against the total
-            # disk usage.
-            continue
-        for filename in filenames:
-            full_path = os.path.join(root, filename)
-            actual_file_size = os.stat(full_path).st_size
-            is_within_range = int(min_file_size) <= actual_file_size <= int(max_file_size)
-            assert_that(is_within_range, equal_to(True), actual_file_size)
+    for full_path in get_all_generated_files(context.working_dir):
+        actual_file_size = os.stat(full_path).st_size
+        is_within_range = int(min_file_size) <= actual_file_size <= int(max_file_size)
+        assert_that(is_within_range, equal_to(True), actual_file_size)
 
 
 @then(u'the size of each generated file should be {file_size}')
 def step_impl(context, file_size):
-    for root, _, filenames in os.walk(context.working_dir):
-        if '.metadata' in root:
-            # Don't count the metadata dir against the total
-            # disk usage.
-            continue
-        for filename in filenames:
-            full_path = os.path.join(root, filename)
-            actual_file_size = os.stat(full_path).st_size
-            assert_that(int(file_size), equal_to(actual_file_size))
+    for full_path in get_all_generated_files(context.working_dir):
+        actual_file_size = os.stat(full_path).st_size
+        assert_that(int(file_size), equal_to(actual_file_size))
